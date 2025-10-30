fe39fbfe7f205546954f5bfb9bbadf7c2b8bd02d
Rutikap07 <151369128+Rutikap07@users.noreply.github.com>
2025-10-30 17:29:35 +0530

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -4,11 +4,11 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
+      host: env('DATABASE_HOST', '193.203.184.65'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
+      database: env('DATABASE_NAME', 'u859302333_strapi'),
+      user: env('DATABASE_USERNAME', 'u859302333_strapi'),
+      password: env('DATABASE_PASSWORD', 'Zaahid97!'),
       ssl: env.bool('DATABASE_SSL', true) ? { rejectUnauthorized: false } : false,
     },
     pool: {
